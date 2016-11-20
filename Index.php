dd4d9bef2dadbd6afe48c9a13b00abd44742f16a
Nightwind19 <nightwind19@yahoo.com>
2016-11-20 12:13:38 -0500

    Add files via upload

---
+++ Index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE HTML>
+<html>  
+<body>
+</div>
+		
+
+<head>
+<title>Form Input Data</title>
+</head>
+
+<body>
+</br>
+<p>Create New User Account</p>
+</br>
+<a href="Index.php"><button>Create</button></a>
+<a href="Read.php"><button>Read</button></a>
+<a href="Update.php"><button>Update</button></a>
+<a href="Delete.php"><button>Delete</button></a>
+</br>
+</br>
+
+<form action="createEntry.php" method="post">
+  First name:</br>
+  <input type="text" name="firstname"></br></br>
+  Last name:</br>
+  <input type="text" name="lastname"></br></br>
+  Email:</br>
+  <input type="text" name="email"></br></br>
+  Password:</br>
+  <input type="text" name="email"></br></br>
+  <input type="submit">;
+</form>
+</body>
+</html>
