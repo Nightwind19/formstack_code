e73496ddf8391b21328ff245bd1509103c2c1fb7
Nightwind19 <nightwind19@yahoo.com>
2016-11-24 19:23:17 -0500

    Add files via upload

---
+++ createTest.php
@@ -0,0 +1,37 @@
+<?php
+
+
+use PHPUnit\Framework\TestCase;
+require_once("createEntry.php");
+
+class userUpdateTest extends TestCase
+{
+    /**
+     * @return PHPUnit_Extensions_Database_DB_IDatabaseConnection
+     */
+function userUpdate() 
+    {
+		if ($firstname = true)
+		
+        $this->assertTrue($firstname);
+		
+		if ($lastname = true)
+		
+		$this->assertTrue($lastname);
+		
+		if ($email = true)
+		
+		$this->assertTrue($email);
+		
+		if ($password = true)
+		
+		$this->assertTrue($password);
+		
+		
+    }
+	
+
+	
+	
+}
+?>
---
+++ deleteTest.php
@@ -0,0 +1,28 @@
+<?php
+
+
+use PHPUnit\Framework\TestCase;
+require_once("userDeleteInfo.php");
+
+class userUpdateTest extends TestCase
+{
+    /**
+     * @return PHPUnit_Extensions_Database_DB_IDatabaseConnection
+     */
+function userUpdate() 
+    {
+		;
+		
+		if ($email = true)
+		
+		$this->assertTrue($email);
+		
+		
+		
+    }
+	
+
+	
+	
+}
+?>
